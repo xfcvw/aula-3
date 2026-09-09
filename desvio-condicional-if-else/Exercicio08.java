7e405b6eda055bd0d9ab4090beec061b1a4eefce
xfcvw <vinicius666787@gmail.com>
2026-09-09 10:05:48 -0300

    Adiciona Exercicio08.java da lista de if-else

---
+++ desvio-condicional-if-else/Exercicio08.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Exercicio08 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        String senha;
+
+        System.out.println("Digite a senha:");
+        senha = entrada.nextLine();
+
+        if (senha.equals("R10p5")) {
+            System.out.println("Acesso concedido");
+        } else {
+            System.out.println("Acesso negado");
+        }
+
+        entrada.close();
+    }
+}
